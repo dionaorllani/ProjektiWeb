3f93058b353feaf246c4ecc6875843fd3c393330
dionaorllani <diona.orllani@gmail.com>
2022-12-25 20:50:16 +0100

    Project Phase 1-Final

---
+++ script.js
@@ -0,0 +1,16 @@
+//SliderBanner
+var array = [
+    'img/SliderBanner/Banner1.jpg',
+    'img/SliderBanner/Banner2.jpg',
+    'img/SliderBanner/Banner3.jpg'
+]
+var index = 0;
+
+function Slider(){
+    document.getElementById('banner').src = array[index++];
+    if(index == array.length){
+        index = 0;
+    }
+    setTimeout("Slider()", 2000); 
+}
+Slider();
